372a24a209597921ba00b47a67f4f7d2c96aa1fe
KAOUKAB <omaima94@icloud.com>
2019-05-22 13:41:41 +0100

    Chapter model and test implementation

--- PD2/PD2-assignement.2/src/main/java/Chapter.java
+++ PD2/PD2-assignement.2/src/main/java/Chapter.java
@@ -1,13 +1,22 @@
 import java.util.regex.Pattern;
 
-public class Chapter {
+public class Chapter implements Comparable<Chapter> {
 
     private String name;
     private String number;
 
     public Chapter(String name, String number) {
-        this.name = name;
-        this.number = number;
+        if(name == null || number == null) {
+            throw new IllegalArgumentException("Arguments cannot be null");
+        } else {
+                Pattern pattern = Pattern.compile("^\\d.\\d$");
+            if(pattern.matcher(number).find()) {
+                this.name = name;
+                this.number = number;
+            } else {
+                throw new IllegalArgumentException("Number should be 'x.x' where x is a digit");
+            }
+        }
     }
 
     public String getName() {
@@ -24,5 +33,23 @@ public class Chapter {
 
     public void setNumber(String number) {
         this.number = number;
+    }
+
+    @Override
+    public int compareTo(Chapter o) {
+         if (this.name.compareTo(o.getName()) == 1 && this.number.compareTo(o.getNumber()) == 1) {
+             return 1;
+        }
+         return 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.getName().equals(((Chapter) obj).getName()) && this.getNumber().equals(((Chapter) obj).getNumber());
     }
 }
